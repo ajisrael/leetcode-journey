be3bfced3cbd0e538182972ab7ae96025dda264e
Alexander Israels <43039187+ajisrael@users.noreply.github.com>
2024-03-13 15:18:26 -0400

    Create ValidAnagram.java

---
+++ ValidAnagram.java
@@ -0,0 +1,71 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) == 0) {
+                map.remove(c);
+            }
+        }
+
+        return map.isEmpty();
+    }
+
+    // First Answer
+
+// import java.util.Arrays;
+
+    // public boolean isAnagram(String s, String t) {
+    //     if (s.length() != t.length()) {
+    //         return false;
+    //     }
+
+    //     char[] arrayS = s.toCharArray();
+    //     char[] arrayT = t.toCharArray();
+
+    //     Arrays.sort(arrayS);
+    //     Arrays.sort(arrayT);
+
+    //     boolean answer = true;
+    //     for (int i = 0; i < arrayS.length; i++) {
+    //         answer = arrayS[i] == arrayT[i];
+    //     }
+
+    //     return answer;
+    // }
+}
